1a35b5f8c43760806f430b36162516307ba81b5a
jaiganeshprakasam <jaiganesh@ideas2it.com>
2022-03-30 14:56:47 +0530

    Feature: Select city dropdown
     - Dropdown load data from json

--- src/App.js
+++ src/App.js
@@ -1,11 +1,65 @@
+import { useState, Component } from 'react';
+import Select from "react-select";
+import { FixedSizeList as List } from "react-window";
+import cities from './assets/cities-fr.json';
 import './App.css';
+
+const height = 35;
+
+class MenuList extends Component {
+
+  render() {
+    const { options, children, maxHeight, getValue } = this.props;
+    const [value] = getValue();
+    const initialOffset = options.indexOf(value) * height;
+
+    return (
+      <List
+        height={maxHeight}
+        itemCount={children.length}
+        itemSize={height}
+        initialScrollOffset={initialOffset}
+      >
+        {({ index, style }) => <div style={style}>{children[index]}</div>}
+      </List>
+    );
+  }
+}
 
 function App() {
 
+  
+  const [currentCity, setCurrentCity] = useState(cities[0]);
+
+  let customStyles = {
+    option: provided => ({
+      ...provided,
+      color: 'black'
+    }),
+    control: provided => ({
+      ...provided,
+      color: 'black'
+    })
+  }
+
+  const selectedCity = (city) => {
+    setCurrentCity(city);
+  }
 
   return (
     <div className="app">
-      My-Weather
+      <div className='app__header'>
+        <label htmlFor="cities">selectionner votre ville:</label>
+        <div className='app__select'>
+          <Select
+            styles={customStyles}
+            components={{ MenuList }}
+            getOptionLabel={(option) => option.nm}
+            onChange={selectedCity}
+            getOptionValue={(option) => option}
+            options={cities} />
+        </div>
+      </div>
     </div>
   );
 }
